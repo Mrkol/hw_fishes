fd53455ce933d8e1e160763cf1dab76d84104435
Mrkol <roma.sandu@gmail.com>
2018-03-15 17:17:32 +0300

    A small change in rule storage format for convinience's sake.

--- automata.py
+++ automata.py
@@ -1,7 +1,11 @@
 from abc import ABCMeta, abstractmethod, abstractproperty
 from itertools import islice, cycle, chain 
+from copy import deepcopy
 import io
 import re
+
+
+DEBUG = False
 
 class CellularAutomata:
     def __init__(self):
@@ -9,11 +13,13 @@ class CellularAutomata:
         self.rules = []
 
     def Step(self):
-        newfield = self.field
+        newfield = deepcopy(self.field)
         for j in range(0, len(self.field)):
             for i in range(0, len(self.field[j])):
                 h = len(self.field)
                 w = len(self.field[0])
+                #Everything in this chain of transformations is basically
+                #an iterator, therefore it's not as inefficient as it looks.
                 area = islice(cycle(self.field), h + j - 1, h + j + 2)
                 area = (islice(cycle(row), w + i - 1, w + i + 2) for row in area)
                 area = chain.from_iterable(area)
@@ -23,8 +29,12 @@ class CellularAutomata:
 
                 for rule in self.rules:
                     if rule[0] == self.field[j][i] and rule[2].match(area):
+                        if DEBUG:
+                            print(rule[0], " -> ", rule[1], " where ", 
+                                area, " matched ", rule[2].pattern)
                         newfield[j][i] = rule[1]
                         break
+
         self.field = newfield
 
     def FastForward(self, n):
@@ -34,8 +44,11 @@ class CellularAutomata:
     def ReadRules(self, textstream):
         self.rules = []
         for line in textstream:
-            words = line.split()
-            self.rules += [(words[0], words[1], re.compile(' '.join(words[2:])))]
+            line = line.strip()
+            self.rules += [(line[0], line[1], re.compile(line[2:]))]
+
+        if DEBUG:
+            print(self.rules)
 
     def ReadState(self, textstream):
         self.field = []
@@ -48,9 +61,4 @@ class CellularAutomata:
         textstream.write(str(self))
     
     def __str__(self):
-        string = ""
-        for row in self.field:
-            for state in row:
-                string += str(state)
-            string += '\n'
-        return string.strip()
+        return '\n'.join(map(''.join, self.field))
