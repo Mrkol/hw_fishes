ed61bebcb9bf92f36a650067b18b2dc036a4ddb3
Mrkol <roma.sandu@gmail.com>
2018-03-21 00:20:04 +0300

    PEP8 fix.

--- automata.py
+++ automata.py
@@ -1,11 +1,12 @@
 from abc import ABCMeta, abstractmethod, abstractproperty
-from itertools import islice, cycle, chain 
+from itertools import islice, cycle, chain
 from copy import deepcopy
 import io
 import re
 
 
 DEBUG = False
+
 
 class CellularAutomata:
     def __init__(self):
@@ -18,11 +19,12 @@ class CellularAutomata:
             for i in range(0, len(self.field[j])):
                 h = len(self.field)
                 w = len(self.field[0])
-                #Everything in this chain of transformations is basically
-                #an iterator, therefore it's not as inefficient as it looks.
+                # Everything in this chain of transformations is basically
+                # an iterator, therefore it's not as inefficient as it looks.
                 #(still slow as hell)
                 area = islice(cycle(self.field), h + j - 1, h + j + 2)
-                area = (islice(cycle(row), w + i - 1, w + i + 2) for row in area)
+                area = (islice(cycle(row), w + i - 1, w + i + 2)
+                        for row in area)
                 area = chain.from_iterable(area)
                 area = list(area)
                 area = area[0:3] + area[5:6] + area[6:9][::-1] + area[3:4]
@@ -31,8 +33,8 @@ class CellularAutomata:
                 for rule in self.rules:
                     if rule[0] == self.field[j][i] and rule[2].match(area):
                         if DEBUG:
-                            print(rule[0], " -> ", rule[1], " where ", 
-                                area, " matched ", rule[2].pattern)
+                            print(rule[0], " -> ", rule[1], " where ",
+                                  area, " matched ", rule[2].pattern)
                         newfield[j][i] = rule[1]
                         break
 
@@ -60,6 +62,6 @@ class CellularAutomata:
 
     def write_state(self, textstream):
         textstream.write(str(self))
-    
+
     def __str__(self):
         return '\n'.join(map(''.join, self.field))
--- tester.py
+++ tester.py
@@ -7,36 +7,31 @@ testFolder = "./tests"
 rules = "./rules.txt"
 
 tests = [splitext(file)[0]
-	for file in listdir(testFolder) 
-	if isfile(join(testFolder, file)) and splitext(file)[1] == ".in"]
+         for file in listdir(testFolder)
+         if isfile(join(testFolder, file)) and splitext(file)[1] == ".in"]
 
 print("Testing on following files: ", tests)
 
 for file in tests:
-	atmt = automata.CellularAutomata()
+    atmt = automata.CellularAutomata()
 
-	with  open(rules, 'r') as ruleset,\
-		open(join(testFolder, file + ".in"), 'r') as input,\
-			open(join(testFolder, file + ".out"), 'r') as expected,\
-				StringIO() as output:
-		steps = int(input.readline())
+    with open(rules, 'r') as ruleset,\
+            open(join(testFolder, file + ".in"), 'r') as input,\
+        open(join(testFolder, file + ".out"), 'r') as expected,\
+        StringIO() as output:
+        steps = int(input.readline())
 
-		atmt.ReadRules(ruleset)
-		atmt.ReadState(input)
-		atmt.FastForward(steps)
-		atmt.WriteState(output)
+        atmt.ReadRules(ruleset)
+        atmt.ReadState(input)
+        atmt.FastForward(steps)
+        atmt.WriteState(output)
 
-		expectedString = expected.read().strip()
-		outputString = output.getvalue().strip()
+        expectedString = expected.read().strip()
+        outputString = output.getvalue().strip()
 
-		if expectedString != outputString:
-			print("Test file \"", file, "\" failed.")
-			print("Test expected:")
-			print(expectedString)
-			print("But resulted in:")
-			print(outputString)
-
-
-
-
-
+        if expectedString != outputString:
+            print("Test file \"", file, "\" failed.")
+            print("Test expected:")
+            print(expectedString)
+            print("But resulted in:")
+            print(outputString)
